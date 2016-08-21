73ab1ac8c4d669b26ad843098a178b47d97513ed
Asnim P Ansari <asnim96@gmail.com>
2016-08-21 11:44:46 +0530

    Created Boarder for snake

--- main.js
+++ main.js
@@ -4,5 +4,13 @@
 window.onload = function () {
     var canvas = document.createElement('canvas');
     ctx = canvas.getContext("2d");
+    canvas.width = 204;
+    canvas.height = 200;
+    var body = document.getElementsByTagName('body')[0];
+    body.appendChild(canvas);
+
+    canvas.lineWidth = 2;//2 Pixel Bordeer Width
+    canvas.strokeStyle = "black";//Black Color
+    ctx.strokeRect(2, 20, canvas.width - 4, canvas.height - 24);
 
 }
