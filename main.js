7779764d21591a568be563c2503e9c4169052552
Asnim P Ansari <asnim96@gmail.com>
2016-08-21 11:32:25 +0530

    initialised Canvas

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
  * Created by Asnim P Ansari on 8/21/2016.
  */
 window.onload = function () {
-    alert("Loaded")
+    var canvas = document.createElement('canvas');
+    ctx = canvas.getContext("2d");
 
 }
