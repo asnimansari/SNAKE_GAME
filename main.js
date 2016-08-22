e4a415bd799b9e7f7886e09e11860860455be54a
Asnim P Ansari <asnim96@gmail.com>
2016-08-22 20:36:00 +0530

    food generated and displayed

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ window.onload = function () {
     var canvas = document.createElement('canvas'),
         ctx = canvas.getContext("2d"),
         score = 0,
-        level  = 0;
+        level  = 0,
+        direction = 0;
     canvas.width = 204;
     canvas.height = 200;
     var body = document.getElementsByTagName('body')[0];
@@ -20,9 +21,28 @@ window.onload = function () {
     function drawGame() {
         console.log("INSIDE DRAWGAME");
 
-        ctx.clearRect(0,0,canvas.width,canvas.height);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        for (var x = 0; x < map.length; x++) {
+            for (var y = 0; y < map[0].length; y++) {
+                console.log("INSIDE MAIN LOOP");
+
+                if (map[x][y] === 1) {
+                    ctx.fillStyle = 'black';
+                    ctx.fillRect(x * 10, y * 10 + 20, 10, 10);
+                }
+                else if (map[x][y] === 2) {
+                    ctx.fillStyle = 'orange';
+                    ctx.fillRect(x * 10, y * 10 + 20, 10, 10);
+                }
+            }
+
+        }
+
+
         drawMain();
     }
+
     function drawMain() {
         ctx.lineWidth = 2;
         ctx.strokeStyle = 'black';
@@ -43,7 +63,20 @@ window.onload = function () {
         }
         map[rndX][rndY] = 1;
         return map;
+    }
+    function generateSnake(map) {
 
+        console.log("INSIDE GENERATE SNAKE");
+        rndX = Math.round(Math.random() * 19);
+        rndY = Math.round(Math.random() * 19);
+        while((rndX - snake.length)<0){
+            rndX = Math.round(Math.random() * 19);
+        }
+        for (var i = 0;i<snake.length;i++){
+            snake[i] = {x:rndX - i,y:rndY};
+            map[rndX -i][rndY] = 2;
+        }
+        return map;
     }
 
 
