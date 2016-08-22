66cd84dc7fce726c345be6248b07cf2842cddf7c
Asnim P Ansari <asnim96@gmail.com>
2016-08-22 20:11:57 +0530

    Function for FoodGeneration Made

--- main.js
+++ main.js
@@ -2,33 +2,42 @@
  * Created by Asnim P Ansari on 8/21/2016.
  */
 window.onload = function () {
-    var canvas = document.createElement('canvas');
-    ctx = canvas.getContext("2d");
+    var canvas = document.createElement('canvas'),
+        ctx = canvas.getContext("2d"),
+        score = 0,
+        level  = 0;
     canvas.width = 204;
     canvas.height = 200;
-    var score = 0;
-    var level = 0;
     var body = document.getElementsByTagName('body')[0];
     body.appendChild(canvas);
-    function drawInitial() {
-        canvas.lineWidth = 2;//2 Pixel Bordeer Width
-        canvas.strokeStyle = "black";//Black Color
-        ctx.strokeRect(2, 20, canvas.width - 4, canvas.height - 24);
-
-        ctx.font = "12px sans-serif";
-        ctx.fillText('SCORE:' + score + 'LEVEL:' + level,2,12)
-
-
-    }
-    drawInitial();
     var map = new Array(20);
-    for(var i = 0;i<map.length;i++){
+    for(var i = 0;i< map.length;i++){
+        console.log("GENERATED MAP");
         map[i] = new Array(20);
     }
-    function foodMaker() {
+    map = generateFood(map);
+    drawGame();
+    function drawGame() {
+        console.log("INSIDE DRAWGAME");
+
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        drawMain();
+    }
+    function drawMain() {
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = 'black';
+        ctx.strokeRect(2,20,canvas.width - 4,canvas.height - 4);
+        ctx.font = "12px sans-serif";
+        ctx.fillText('Score: ' + score + ' - Level '+level,2,12);
+
+
+    }
+    function generateFood(map) {
+        console.log("INSIDE GENERTE FOOD");
+
         var rndX = Math.round(Math.random() * 19),
             rndY = Math.round(Math.random() * 19);
-        while(map[rndX][rndY] === 2){
+        while (map[rndX][rndY] === 2){
             rndX = Math.round(Math.random() * 19);
             rndY = Math.round(Math.random() * 19);
         }
