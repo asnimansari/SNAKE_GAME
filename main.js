75b32c36f18e2227627c3cb250d75dc121faa6e9
Asnim P Ansari <asnim96@gmail.com>
2016-08-25 13:07:11 +0530

    Added PROJECT FILES FOR CANVAS SNAKE

--- main.js
+++ main.js
@@ -86,7 +86,7 @@ window.onload = function ()
         }
         for(var i = 0;i< fence.length;i++){
             if (snake_head.x === fence[i].x && snake_head.y === fence[i].y){
-                console.log("HIT");r
+                console.log("HIT");
                 clearInterval(interval_id);
             }
         }
