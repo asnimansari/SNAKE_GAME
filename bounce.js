439c722da25134fac3f54a7f516005a32c6a032e
Asnim P Ansari <asnim96@gmail.com>
2016-09-10 20:55:03 +0530

    PREVENTED BOTTOM BOUNCE AND ADDED PADDLE HIT

--- bounce.js
+++ bounce.js
@@ -4,18 +4,16 @@
 
 window.onload = function () {
 
-
     var canvas = document.getElementById("bounceCanvas");
     var ctx = canvas.getContext("2d");
     var ballRadius = 10;
     var x = canvas.width/2;
-    var y = canvas.height - 30;
+    var y = canvas.height-30;
     var dx = 2;
     var dy = -2;
     var paddleHeight = 10;
     var paddleWidth = 75;
     var paddleX = (canvas.width-paddleWidth)/2;
-
     var rightPressed = false;
     var leftPressed = false;
 
@@ -39,8 +37,13 @@ window.onload = function () {
         }
     }
 
-
-    //FUNCTION TO DRAW BALL
+    function drawBall() {
+        ctx.beginPath();
+        ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+    }
     function drawPaddle() {
         ctx.beginPath();
         ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
@@ -48,29 +51,44 @@ window.onload = function () {
         ctx.fill();
         ctx.closePath();
     }
-    function drawBall() {
-        ctx.beginPath();
-        ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-        ctx.fillStyle = "#0095DD";
-        ctx.fill();
-        ctx.closePath();
-
-    }
 
     function draw() {
-        ctx.clearRect(0,0,canvas.width,canvas.height);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
         drawBall();
         drawPaddle();
-        //DETECT AND REVERSE THE DIRECTION IF HIT OCCURED
+
         if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
             dx = -dx;
         }
-        if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        if(y + dy < ballRadius) {
             dy = -dy;
         }
+        else  if (y + dy > canvas.height-ballRadius){
+            if(x > paddleX && x < paddleX + paddleWidth) {
+                dy = -dy;
+            }
+            else {
+                alert("GAME OVER");
+                document.location.reload();
+            }
+        }
+
+        if(rightPressed && paddleX < canvas.width-paddleWidth) {
+            paddleX += 7;
+        }
+        else if(leftPressed && paddleX > 0) {
+            paddleX -= 7;
+        }
+
         x += dx;
         y += dy;
-
     }
+
     setInterval(draw, 10);
+
+
 }
+
+
+
+
