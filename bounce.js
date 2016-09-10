b2d4a57a6ad09c1a4e168079f420c78a3dc83cb5
Asnim P Ansari <asnim96@gmail.com>
2016-09-10 20:38:15 +0530

    added bounce

--- bounce.js
+++ bounce.js
@@ -7,6 +7,7 @@ window.onload = function () {
 
     var canvas = document.getElementById("bounceCanvas");
     var ctx = canvas.getContext("2d");
+    var ballRadius = 10;
     var x = canvas.width/2;
     var y = canvas.height - 30;
     var dx = 2;
@@ -14,7 +15,7 @@ window.onload = function () {
 
     function drawBall() {
         ctx.beginPath();
-        ctx.arc(x, y, 10, 0, Math.PI * 2);
+        ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
         ctx.fillStyle = "#0095DD";
         ctx.fill();
         ctx.closePath();
@@ -24,6 +25,13 @@ window.onload = function () {
     function draw() {
         ctx.clearRect(0,0,canvas.width,canvas.height);
         console.log("FDREA");
+        drawBall();
+        if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+            dx = -dx;
+        }
+        if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+            dy = -dy;
+        }
 
         x += dx;
         y += dy;
