cbe3755cf4c5357e203188bf559a0fd733ea7120
Asnim <asnim96@gmail.com>
2019-06-30 13:13:58 +0530

    Refactored OLD JS Code

--- main.js
+++ main.js
@@ -1,28 +1,37 @@
 /**
  * Created by Asnim P Ansari on 8/21/2016.
  */
+
+let REFRESH_DELAY = 150;
+const SNAKE_POSITION_OFFSET = 20;
 window.onload = function() {
+  let i;
   // INITIALISING CANVAS AND OTHER COMPONENTS REQUIRED
-  var canvas = document.getElementById('snakecanvas');
-  var canvas1 = document.getElementById('snakescoreboard');
-  var ctx1 = canvas1.getContext('2d');
-  var ctx = canvas.getContext('2d');
+  const snakecanvas = document.getElementById('snakecanvas');
+  const scoreboardCanvas = document.getElementById('snakescoreboard');
+  const snakeGameContext = scoreboardCanvas.getContext('2d');
+  const scoreBoardContext = snakecanvas.getContext('2d');
+  const SnakeDirections = { UP: 4, DOWN: 2, LEFT: 3, RIGHT: 1 };
+
+  const audioButtonPress = new Audio('audio/btn_press.mp3');
+  const audioFoodCaptured = new Audio('audio/food_captured.mp3');
+  const audioWallHit = new Audio('audio/wall_hit.mp3');
+  const audioSelfBite = new Audio('audio/self_bite.mp3');
 
   // INITIALISED SNAKE
-  var snake = [{ x: 40, y: 200 }, { x: 60, y: 200 }, { x: 80, y: 200 }];
-  var direction = 1;
-  var rndX = 0,
-    rndY = 0;
-  var REFRESH_DELAY = 150;
-  var GAME_LEVEL = 0;
+  let snake = [{ x: 40, y: 200 }, { x: 60, y: 200 }, { x: 80, y: 200 }];
+
+  //Sets Initial Snake Moving Direction
+  let direction = SnakeDirections.RIGHT;
+
+  let foodLocationX = 0;
+  let foodLocationY = 0;
+
+  let gameLevel = 0;
 
   var fence = new Array(8);
 
   //AUDIO INITIALISATION FOR THE GAME
-  var aud_btnPress = new Audio('audio/btn_press.mp3');
-  var aud_foodCaptured = new Audio('audio/food_captured.mp3');
-  var aud_wallHit = new Audio('audio/wall_hit.mp3');
-  var aud_selfBite = new Audio('audio/self_bite.mp3');
 
   //AUTOMATIC FENCE GENERATION FOR EACH LEVEL
   for (i = 0; i < fence.length; i++) {
@@ -30,14 +39,14 @@ window.onload = function() {
   }
   fence[0] = [];
   // BLANK FENCE 1
-  for (var i = 0; i < 48; i++) {
+  for (i = 0; i < 48; i++) {
     fence[1].push({ x: i * 20, y: 0 }, { x: i * 20, y: 620 });
   }
-  for (var i = 1; i < 31; i++) {
+  for (i = 1; i < 31; i++) {
     fence[1].push({ y: i * 20, x: 0 }, { y: i * 20, x: 940 });
   }
   //LEVEL 2 FENCE
-  for (var i = 0; i < 7; i++) {
+  for (i = 0; i < 7; i++) {
     fence[2].push(
       { x: i * 20, y: 0 },
       { x: 940 - i * 20, y: 0 },
@@ -46,30 +55,30 @@ window.onload = function() {
       { x: 0, y: i * 20 },
       { x: 0, y: 620 - i * 20 },
       { x: 940, y: i * 20 },
-      { x: 940, y: 620 - i * 20 }
+      { x: 940, y: 620 - i * 20 },
     );
   }
-  shift_X = 220;
-  shift_Y = 200;
-  for (var i = 0; i < 25; i++) {
+  const level2ShiftX = 220;
+  const level2ShiftY = 200;
+  for (i = 0; i < 25; i++) {
     fence[2].push(
-      { x: i * 20 + shift_X, y: shift_Y },
-      { x: i * 20 + shift_X, y: 620 - shift_Y }
+      { x: i * 20 + level2ShiftX, y: level2ShiftY },
+      { x: i * 20 + level2ShiftX, y: 620 - level2ShiftY },
     );
   }
   // LEVEL 3  FENCE
-  var level_3_shiftX = 300;
-  var level_3_shiftY = 100;
-  for (var i = 0; i < 20; i++) {
+  const level3ShiftX = 300;
+  const level3ShiftY = 100;
+  for (i = 0; i < 20; i++) {
     fence[3].push(
-      { x: level_3_shiftX, y: 620 - i * 20 },
-      { x: 940 - level_3_shiftX, y: i * 20 }
+      { x: level3ShiftX, y: 620 - i * 20 },
+      { x: 940 - level3ShiftX, y: i * 20 },
     );
   }
-  for (var i = 0; i < 20; i++) {
+  for (i = 0; i < 20; i++) {
     fence[3].push(
-      { x: i * 20, y: level_3_shiftY },
-      { x: 940 - i * 20, y: 620 - level_3_shiftY }
+      { x: i * 20, y: level3ShiftY },
+      { x: 940 - i * 20, y: 620 - level3ShiftY },
     );
   }
   //LEVEL 4 FENCE
@@ -84,63 +93,63 @@ window.onload = function() {
       { x: 940, y: 620 - i * 20 },
     );
   }
-  shift_X = 300;
-  shift_Y = 100;
-  Y_Offet = 120;
-  for (var i = 0; i < 20; i++) {
+  const level4ShiftX = 300;
+  const level4ShiftY = 100;
+  const Y_Offet = 120;
+  for (i = 0; i < 20; i++) {
     fence[4].push(
-      { x: +shift_X, y: shift_Y + Y_Offet + i * 20 },
-      { x: 940 - shift_X, y: shift_Y + i * 20 }
+      { x: +level4ShiftX, y: level4ShiftY + Y_Offet + i * 20 },
+      { x: 940 - level4ShiftX, y: level4ShiftY + i * 20 },
     );
   }
   // LEVEL 5 FENCE HORIZONTALS
-  for (var i = 0; i < 10; i++) {
+  for (i = 0; i < 10; i++) {
     fence[5].push({ x: i * 20, y: 0 });
   }
-  for (var i = 16; i < 35; i++) {
+  for (i = 16; i < 35; i++) {
     fence[5].push({ x: i * 20, y: 0 });
   }
-  for (var i = 0; i < 26; i++) {
+  for (i = 0; i < 26; i++) {
     fence[5].push({ x: i * 20, y: 260 });
   }
-  for (var i = 34; i < 48; i++) {
+  for (i = 34; i < 48; i++) {
     fence[5].push({ x: i * 20, y: 260 });
   }
-  for (var i = 0; i < 48; i++) {
+  for (i = 0; i < 48; i++) {
     fence[5].push({ x: i * 20, y: 460 });
   }
   //VERTICALS
-  for (var i = 1; i <= 8; i++) {
+  for (i = 1; i <= 8; i++) {
     fence[5].push({ x: 0, y: i * 20 });
   }
-  for (var i = 1; i <= 12; i++) {
+  for (i = 1; i <= 12; i++) {
     fence[5].push({ x: 500, y: i * 20 });
   }
-  for (var i = 1; i <= 12; i++) {
+  for (i = 1; i <= 12; i++) {
     fence[5].push({ x: 700, y: 460 + i * 20 });
   }
   //LEVEL 6 FENCE
-  for (var i = 0; i < 48; i++) {
+  for (i = 0; i < 48; i++) {
     fence[6].push({ x: i * 20, y: 320 });
   }
-  for (var i = 0; i < 48; i++) {
+  for (i = 0; i < 48; i++) {
     fence[6].push({ x: 460, y: i * 20 });
   }
   //LEVEL 7 FENCE
-  for (var i = 0; i < 48; i++) {
+  for (i = 0; i < 48; i++) {
     fence[7].push({ x: i * 20, y: 420 });
   }
-  for (var i = 0; i < 11; i++) {
+  for (i = 0; i < 11; i++) {
     fence[7].push({ x: 240, y: 420 + i * 20 }, { x: 740, y: 420 + i * 20 });
   }
   // END OF FENCE GENERATION SECTION
 
-  //CO-ORIDATES FOR SHOWING SNAKE game_over
-  var game_over = new Array();
+  //CO-ORDINATES FOR SHOWING SNAKE game_over
+  const gameOver = new Array();
 
   //GENRERATE 5 PIX HORIZONTALS
-  for (var i = 0; i < 5; i++) {
-    game_over.push(
+  for (i = 0; i < 5; i++) {
+    gameOver.push(
       { x: 200 + i * 20, y: 20 },
       { x: 200 + i * 20, y: 140 },
       { x: 40 + i * 20, y: 360 },
@@ -148,23 +157,23 @@ window.onload = function() {
       { x: 40 + i * 20, y: 20 },
       { x: 40 + i * 20, y: 260 },
       { x: 40 + i * 20, y: 140 },
-      { x: 140, y: 160 + i * 20 }
+      { x: 140, y: 160 + i * 20 },
     );
   }
 
   //GENRERATE 12 PIX HORIZONTALS
-  for (var i = 0; i < 12; i++) {
-    game_over.push({ x: 180, y: i * 20 + 40 }, { x: 300, y: i * 20 + 40 });
+  for (i = 0; i < 12; i++) {
+    gameOver.push({ x: 180, y: i * 20 + 40 }, { x: 300, y: i * 20 + 40 });
   }
 
   //GENRERATE 11 PIX HORIZONTALS
-  for (var i = 0; i < 11; i++) {
-    game_over.push({ x: 20, y: i * 20 + 40 });
+  for (i = 0; i < 11; i++) {
+    gameOver.push({ x: 20, y: i * 20 + 40 });
   }
 
   //13 pixel vertials
-  for (var i = 0; i < 13; i++) {
-    game_over.push(
+  for (i = 0; i < 13; i++) {
+    gameOver.push(
       { x: 340, y: i * 20 + 20 },
       { x: 500, y: i * 20 + 20 },
       { x: 340 + i * 6, y: i * 20 + 20 },
@@ -175,12 +184,12 @@ window.onload = function() {
       { x: 180 + i * 6, y: 360 + i * 20 },
       { x: 340 - i * 6, y: 360 + i * 20 },
       { x: 380, y: 360 + i * 20 },
-      { x: 520, y: 360 + i * 20 }
+      { x: 520, y: 360 + i * 20 },
     );
   }
   //GERERATE 6 PIXESLS
-  for (var i = 0; i < 6; i++) {
-    game_over.push(
+  for (i = 0; i < 6; i++) {
+    gameOver.push(
       { x: 560 + i * 20, y: 20 },
       { x: 560 + i * 20, y: 140 },
       { x: 560 + i * 20, y: 260 },
@@ -191,27 +200,28 @@ window.onload = function() {
       { x: 520 + i * 20, y: 460 },
       { x: 620, y: 360 + i * 20 },
       { x: 520 + i * 20, y: 480 + i * 20 },
-      { x: 620, y: 600 }
+      { x: 620, y: 600 },
     );
   }
 
   //SHIFT POSTION OF GAME OVER
-  for (var i = 0; i < game_over.length; i++) {
-    game_over[i].x += 120;
+  for (i = 0; i < gameOver.length; i++) {
+    gameOver[i].x += 120;
   }
 
   // SNAKEDIRECTION DICTIONARY CREATIION
-  var SnakeDirections = { UP: 4, DOWN: 2, LEFT: 3, RIGHT: 1 };
+
   // SCORE  AND DISPLAY LEVEL INITIALISATION
-  score = 0;
-  level = 1;
-  // KEYPRESS AND EXECUTION FLAG( USED TO PREVENT MUTLIPLE STROKES WHICH LEADS TO SNAKE BIT )
-  var key_executed = true;
+  let score = 0;
+  let level = 1;
+  // KEYPRESS AND EXECUTION FLAG( USED TO PREVENT MULTIPLE STROKES WHICH LEADS TO SNAKE BIT )
+  let key_executed = true;
 
   // ARROW KEY LISTNER FOR SNAKE CONTROLS
   document.addEventListener('keydown', function(e) {
-    var keyCode = e.keyCode;
-    keyList = { 39: false, 40: false, 37: false, 38: false };
+    const keyCode = e.keyCode;
+    console.log(keyCode);
+    const keyList = { 39: false, 40: false, 37: false, 38: false };
     keyList[keyCode] = true;
     // PRECAUTION TO PREVENT HITTING MUTLIPLE KEY( IMPLIMENTED A BOOLEAN FUNCTION WITH VARIABLES WHICH SHOWS TRUE WHEN ONLY ONE VARIABLE IS TRUE)
     // USED HERE TO DETECT WHETHER TWO KEYS HAS BEEN STRUCK TOGETHER
@@ -224,7 +234,7 @@ window.onload = function() {
       if (keyCode === 39 && direction != SnakeDirections.LEFT && key_executed) {
         key_executed = false;
         direction = SnakeDirections.RIGHT;
-        aud_btnPress.play();
+        audioButtonPress.play();
       } else if (
         keyCode === 40 &&
         direction != SnakeDirections.UP &&
@@ -232,7 +242,7 @@ window.onload = function() {
       ) {
         key_executed = false;
         direction = SnakeDirections.DOWN;
-        aud_btnPress.play();
+        audioButtonPress.play();
       } else if (
         keyCode === 37 &&
         direction != SnakeDirections.RIGHT &&
@@ -240,7 +250,7 @@ window.onload = function() {
       ) {
         key_executed = false;
         direction = SnakeDirections.LEFT;
-        aud_btnPress.play();
+        audioButtonPress.play();
       } else if (
         keyCode === 38 &&
         direction != SnakeDirections.DOWN &&
@@ -248,7 +258,7 @@ window.onload = function() {
       ) {
         key_executed = false;
         direction = SnakeDirections.UP;
-        aud_btnPress.play();
+        audioButtonPress.play();
       }
     }
   });
@@ -256,12 +266,12 @@ window.onload = function() {
 
   // ANIMATE FUNCTION IS USED TO REFRESH THE CANVAS
   function animate() {
-    ctx.clearRect(0, 0, 959, 639);
-    ctx.fillStyle = '#384619';
+    scoreBoardContext.clearRect(0, 0, 959, 639);
+    scoreBoardContext.fillStyle = '#384619';
 
     // LOOP FOR DRAWING SNAKE
     for (var i = 0; i < snake.length; i++) {
-      ctx.fillRect(snake[i].x, snake[i].y, 19, 19);
+      scoreBoardContext.fillRect(snake[i].x, snake[i].y, 19, 19);
 
       // IF ELSE SECTION TO DETECT WHETHER THE SNAKE HEAD HAS PASSED ORIGIN
       // AND SET APPROPIRATE  CO ORDIATES
@@ -280,33 +290,33 @@ window.onload = function() {
     }
     snakeFoodDraw();
     snake.shift(); // USED TO MOVE SNAKE
-    ctx1.background = '#000000';
-    ctx1.font = '20px sans-serif';
-    ctx1.clearRect(0, 0, 959, 100);
-    ctx1.fillText('SCORE BOARD :' + score, 15, 40);
-    ctx1.fillText('LEVEL :' + level, 15, 80);
+    snakeGameContext.background = '#000000';
+    snakeGameContext.font = '20px sans-serif';
+    snakeGameContext.clearRect(0, 0, 959, 100);
+    snakeGameContext.fillText('SCORE BOARD :' + score, 15, 40);
+    snakeGameContext.fillText('LEVEL :' + level, 15, 80);
     snakeshift();
     key_executed = true;
     var snake_head = {
       x: snake[snake.length - 1].x,
       y: snake[snake.length - 1].y,
     };
-    if (snake_head.x === rndX && snake_head.y === rndY) {
+    if (snake_head.x === foodLocationX && snake_head.y === foodLocationY) {
       console.log('Passed Food');
-      aud_foodCaptured.play();
+      audioFoodCaptured.play();
       snakeshift();
       score = score + 1;
       foodMaker();
       if (score % 10 == 0) {
         snake = [{ x: 40, y: 180 }, { x: 60, y: 180 }, { x: 80, y: 180 }];
         direction = SnakeDirections.RIGHT;
-        GAME_LEVEL = GAME_LEVEL + 1;
+        gameLevel = gameLevel + 1;
         foodMaker();
-        if (GAME_LEVEL === 8) {
-          GAME_LEVEL = 0;
+        if (gameLevel === 8) {
+          gameLevel = 0;
           REFRESH_DELAY = REFRESH_DELAY - 20;
-          clearInterval(interval_id);
-          interval_id = setInterval(animate, REFRESH_DELAY);
+          clearInterval(animationSetIntervalID);
+          animationSetIntervalID = setInterval(animate, REFRESH_DELAY);
           level = level + 1;
         }
       }
@@ -316,89 +326,93 @@ window.onload = function() {
         wallHit();
       }
     }
-    for (var i = 0; i < fence[GAME_LEVEL].length; i++) {
+    for (var i = 0; i < fence[gameLevel].length; i++) {
       if (
-        snake_head.x === fence[GAME_LEVEL][i].x &&
-        snake_head.y === fence[GAME_LEVEL][i].y
+        snake_head.x === fence[gameLevel][i].x &&
+        snake_head.y === fence[gameLevel][i].y
       ) {
         selfBite();
       }
     }
-    for (var i = 0; i < fence[GAME_LEVEL].length; i++) {
-      ctx.fillRect(fence[GAME_LEVEL][i].x, fence[GAME_LEVEL][i].y, 19, 19);
+    for (var i = 0; i < fence[gameLevel].length; i++) {
+      scoreBoardContext.fillRect(
+        fence[gameLevel][i].x,
+        fence[gameLevel][i].y,
+        19,
+        19,
+      );
     }
   }
-  // showgame_overScreen();
-  interval_id = setInterval(animate, REFRESH_DELAY);
-  // interval_id = requestAnimationFrame(animate);
+  // showGameOverScreen();
+  let animationSetIntervalID = setInterval(animate, REFRESH_DELAY);
+  // animationSetIntervalID = requestAnimationFrame(animate);
   function snakeshift() {
-    last_pos = snake[snake.length - 1];
     last_pos = snake[snake.length - 1];
     switch (direction) {
       case 1:
-        snake.push({ x: last_pos.x + 20, y: last_pos.y });
+        snake.push({ x: last_pos.x + SNAKE_POSITION_OFFSET, y: last_pos.y });
         break;
       case 2:
-        snake.push({ x: last_pos.x, y: last_pos.y + 20 });
+        snake.push({ x: last_pos.x, y: last_pos.y + SNAKE_POSITION_OFFSET });
         break;
       case 3:
-        snake.push({ x: last_pos.x - 20, y: last_pos.y });
+        snake.push({ x: last_pos.x - SNAKE_POSITION_OFFSET, y: last_pos.y });
         break;
       case 4:
-        snake.push({ x: last_pos.x, y: last_pos.y - 20 });
+        snake.push({ x: last_pos.x, y: last_pos.y - SNAKE_POSITION_OFFSET });
         break;
     }
   }
   function foodMaker() {
-    (rndX = Math.round(Math.random() * 38) * 20),
-      (rndY = Math.round(Math.random() * 38) * 20);
-    console.log(rndX, rndY);
-    while (rndX > 920 || rndY > 600) {
-      rndX = Math.round(Math.random() * 38) * 20;
-      rndY = Math.round(Math.random() * 38) * 20;
+    (foodLocationX = Math.round(Math.random() * 38) * 20),
+      (foodLocationY = Math.round(Math.random() * 38) * 20);
+    console.log(foodLocationX, foodLocationY);
+    while (foodLocationX > 920 || foodLocationY > 600) {
+      foodLocationX = Math.round(Math.random() * 38) * 20;
+      foodLocationY = Math.round(Math.random() * 38) * 20;
     }
 
     try {
-      var total_length = fence[GAME_LEVEL].length || 0;
+      var total_length = fence[gameLevel].length || 0;
     } catch (err) {
       var total_length = 0;
     }
     for (i = 0; i < total_length; i++) {
-      if (fence[GAME_LEVEL][i].x === rndX && fence[GAME_LEVEL][i].y === rndY) {
-        rndX = Math.round(Math.random() * 38) * 20;
-        rndY = Math.round(Math.random() * 38) * 20;
+      if (
+        fence[gameLevel][i].x === foodLocationX &&
+        fence[gameLevel][i].y === foodLocationY
+      ) {
+        foodLocationX = Math.round(Math.random() * 38) * 20;
+        foodLocationY = Math.round(Math.random() * 38) * 20;
         i = 0;
-        console.log('IN FENSE');
       }
-      console.log('LOOPING');
     }
   }
   function snakeFoodDraw() {
-    // Changed Shape of Food
-    ctx.fillRect(rndX + 6, rndY, 6, 6);
-    ctx.fillRect(rndX + 6, rndY + 12, 6, 6);
-    ctx.fillRect(rndX, rndY + 6, 6, 6);
-    ctx.fillRect(rndX + 12, rndY + 6, 6, 6);
+    // Draws Snake Food On Screen
+    scoreBoardContext.fillRect(foodLocationX + 6, foodLocationY, 6, 6);
+    scoreBoardContext.fillRect(foodLocationX + 6, foodLocationY + 12, 6, 6);
+    scoreBoardContext.fillRect(foodLocationX, foodLocationY + 6, 6, 6);
+    scoreBoardContext.fillRect(foodLocationX + 12, foodLocationY + 6, 6, 6);
   }
   function wallHit() {
-    console.log('HIT');
-    aud_wallHit.play();
-    setTimeout(showgame_overScreen, 2000);
-    // showgame_overScreen();
-    clearInterval(interval_id);
+    audioWallHit.play();
+    setTimeout(showGameOverScreen, 2000);
+
+    clearInterval(animationSetIntervalID);
   }
 
   function selfBite() {
-    aud_selfBite.play();
-    console.log('HIT');
-    setTimeout(showgame_overScreen, 2000);
-    clearInterval(interval_id);
+    audioSelfBite.play();
+
+    setTimeout(showGameOverScreen, 2000);
+    clearInterval(animationSetIntervalID);
   }
-  function showgame_overScreen() {
-    ctx.clearRect(0, 0, 959, 639);
-    ctx.fillStyle = '#384619';
-    for (i = 0; i < game_over.length; i++) {
-      ctx.fillRect(game_over[i].x, game_over[i].y, 19, 19);
+  function showGameOverScreen() {
+    scoreBoardContext.clearRect(0, 0, 959, 639);
+    scoreBoardContext.fillStyle = '#384619';
+    for (i = 0; i < gameOver.length; i++) {
+      scoreBoardContext.fillRect(gameOver[i].x, gameOver[i].y, 19, 19);
     }
   }
 };
